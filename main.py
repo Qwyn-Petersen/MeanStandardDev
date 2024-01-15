045c0155ed54675789cf0c030972b80100a25e20
Qwyn-Petersen <156146563+Qwyn-Petersen@users.noreply.github.com>
2024-01-15 01:52:34 +0000

    2nd commit

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 def find_mean():
   
   print("Welcome to the mean calculator!"'\n')
-  # Set a variable for the total amount of numbers
   total_num = 0
-  # Initialize the sum to 0
   sum = 0
-  # Initialize a bool for the while loop
   switch = True
-  # Loop through the numbers and add them to the sum
   
+  # Loop through the numbers and add them to the sum
   while(switch):
     num = (float(input("To coutinue, enter a postive # or enter '-1' to stop: ")))
+    if num == '':
+      print("Invalid input. Please enter a valid number.")
+      continue
     if num > 0: 
       sum += num
       total_num += 1 
@@ -20,30 +20,44 @@ def find_mean():
   # Calculate the mean and print it
   if total_num > 0:
     mean = sum / total_num
-    print("The mean is:", mean)
+    print("The mean is: ", mean,'\n')
+    return mean
   else:
     print("No numbers were entered.")
+ 
 
-def find_stand_dev():
-  # Get the number of numbers to be averaged
-  int(input("How many numbers do want to use? "))
-  
+def find_stand_dev(mean):
 
+  print("Welcome to the standard deviation calculator!"'\n')
+  total_num = 0
+  sum = 0
+  switch = True
 
+  while(switch):
+    num = (float(input("To coutinue, enter a postive # or enter '-1' to stop: ")))
+    if num == '':
+      print("Invalid input. Please enter a valid number.")
+      continue
+    if num > 0:
+      dev = abs((mean - num) ** 2)
+      sum += dev
+      total_num += 1
+    else:
+      switch = False
 
-
-
-
-
+  if total_num > 0:
+      standard_dev = sum / total_num
+      print("The standard deviation is:", standard_dev,'\n')
+      return standard_dev
+  else:
+      print("No numbers were entered.")
+      
 def main():
-  find_mean()
+  mean = find_mean()
+  standard_dev = find_stand_dev(mean)
+  print("The mean is: ", mean)
+  print("The standard deviation is:", standard_dev)
   
-  
-
-
-
-
-
 
 
 
